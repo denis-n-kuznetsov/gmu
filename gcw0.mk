17d82c7f71c6487d2c58a53d60d3b68c3331052b
Denis Kuznetsov <denis.n.kuznetsov@gmail.com>
2019-02-08 17:46:54 +0300

    Adapted GCW0 configs to GMU 0.10.1.

--- gcw0.mk
+++ gcw0.mk
@@ -14,21 +14,40 @@
 # for details.
 #
 
-DECODERS_TO_BUILD  = decoders/vorbis.so decoders/flac.so decoders/wavpack.so\
-                     decoders/mpg123.so decoders/mikmod.so
-FRONTENDS_TO_BUILD = frontends/sdl.so frontends/log.so frontends/gmuhttp.so
+DECODERS_TO_BUILD  = \
+                     decoders/vorbis.so \
+                     decoders/flac.so \
+                     decoders/mikmod.so \
+                     decoders/mpg123.so \
+
+FRONTENDS_TO_BUILD = \
+                     frontends/sdl.so \
+                     frontends/gmuhttp.so \
+
 GCW0_SDK          ?= /opt/gcw0-toolchain/
-SYSROOT_USR        = usr/mipsel-gcw0-linux-uclibc/sysroot/usr/
+SYSROOT_BIN        = usr/mipsel-gcw0-linux-uclibc/sysroot/usr/bin/
 TOOLS_TO_BUILD     =
-SDL_LIB            = `$(GCW0_SDK)$(SYSROOT_USR)bin/sdl-config --libs`
-SDL_CFLAGS         = `$(GCW0_SDK)$(SYSROOT_USR)bin/sdl-config --cflags`
-CXX                = mipsel-linux-g++
-CC                 = mipsel-linux-gcc
-STRIP              = mipsel-linux-strip
-COPTS             ?= -O2 -ffast-math
-CFLAGS             = -fomit-frame-pointer $(SDL_CFLAGS) \
-                     -I$(GCW0_SDK)include/ -DFILE_HW_H="\"hw_$(TARGET).h\""
-LFLAGS             = -s $(SDL_LIB) -lm -ldl -lz -lgcc -Wl,-export-dynamic
-DISTFILES          = $(BINARY) frontends decoders themes htdocs libs.gcw0 \
-                     gmu.bmp gmu.png README.txt gmu-gcw0.dge COPYING \
-                     gmuinput.gcw0.conf default.gcw0.desktop
+SDL_LIB            = `$(GCW0_SDK)$(SYSROOT_BIN)sdl-config --libs`
+SDL_CFLAGS         = `$(GCW0_SDK)$(SYSROOT_BIN)sdl-config --cflags`
+CXX                = $(GCW0_SDK)usr/bin/mipsel-linux-g++
+CC                 = $(GCW0_SDK)usr/bin/mipsel-linux-gcc
+STRIP              = $(GCW0_SDK)usr/bin/mipsel-linux-strip
+
+COPTS             ?= -O2
+
+CFLAGS             = \
+                     $(SDL_CFLAGS) \
+                     -fomit-frame-pointer -ffunction-sections -ffast-math \
+                     -I$(GCW0_SDK)include/ \
+                     -DFILE_HW_H="\"hw_gcw0.h\"" \
+
+LFLAGS             = \
+                     $(SDL_LIB) \
+                     -lm -ldl -lz -lgcc -Wl,-export-dynamic \
+
+DISTFILES          = \
+                     $(BINARY) frontends decoders themes \
+                     gmu.bmp gmu.png README.txt COPYING \
+                     htdocs \
+                     gmu-gcw0.dge gmuinput.gcw0.conf default.gcw0.desktop \
+
